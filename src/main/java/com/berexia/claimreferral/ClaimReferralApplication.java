5f7058aa900ea6ec22cadb0097ab726f18b41f61
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-07 18:22:57 +0000

    update claim referral ...

---
+++ src/main/java/com/berexia/claimreferral/ClaimReferralApplication.java
@@ -0,0 +1,13 @@
+package com.berexia.claimreferral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimReferralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaimReferralApplication.class, args);
+    }
+
+}
